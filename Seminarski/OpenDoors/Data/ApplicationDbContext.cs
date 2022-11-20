98f3e57475a81e1f1aa68ca7798896777b4aedcb
aminaM24 <94532479+aminaM24@users.noreply.github.com>
2022-11-20 16:35:13 +0100

    Update ApplicationDbContext.cs

--- Seminarski/OpenDoors/Data/ApplicationDbContext.cs
+++ Seminarski/OpenDoors/Data/ApplicationDbContext.cs
@@ -1,6 +1,30 @@
-﻿namespace OpenDoors.Data
+using OpenDoors.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Drawing;
+using OpenDoors.Models;
+using System.Data.Entity;
+
+namespace OpenDoors.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public DbSet<Email> Email { get; set; }
+        public DbSet<Grad> Grad { get; set; }
+        public DbSet<KorisnickiNalog> KorisnickiNalog { get; set; }
+        public DbSet<Korisnik> Korisnik { get; set; }
+        public DbSet<KreditnaKartica> KreditnaKartica { get; set; }
+        public DbSet<Lokacija> Lokacija { get; set; }
+        
+
+
+        public ApplicationDbContext(
+            DbContextOptions options) : base(options)
+        {
+        }
+
     }
 }
+
+
+
