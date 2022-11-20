ca86c442bb5f76bca9680ad4c69711681b886ffe
Amna Macić <106256903+amnamacic@users.noreply.github.com>
2022-11-20 21:04:20 +0100

    Update TipNekretnine.cs

--- Seminarski/OpenDoors/Models/TipNekretnine.cs
+++ Seminarski/OpenDoors/Models/TipNekretnine.cs
@@ -1,6 +1,14 @@
-﻿namespace OpenDoors.Models
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OpenDoors.Models
 {
     public class TipNekretnine
     {
+        [Key]
+        public int Id { get; set; }
+        public string Tip { get; set; }
+        public string Opis { get; set; }
+        public List<Nekretnina> Nekretnina { get; set; }
     }
 }
