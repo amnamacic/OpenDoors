52e449801ea1ce7ab3b12586f290379e420f7276
Amna Macić <106256903+amnamacic@users.noreply.github.com>
2022-11-20 21:03:03 +0100

    Update Recenzije.cs

--- Seminarski/OpenDoors/Models/Recenzije.cs
+++ Seminarski/OpenDoors/Models/Recenzije.cs
@@ -1,6 +1,24 @@
-﻿namespace OpenDoors.Models
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OpenDoors.Models
 {
     public class Recenzije
     {
+        [Key]
+        public int Id { get; set; }
+        public DateTime DatumPostavljanja { get; set; }
+        public int Ocjena { get; set; }
+        public string Komentar { get; set; }
+        public DateTime DatumModifikacije { get; set; }
+        [ForeignKey("NekretninaId")]
+        public Nekretnina Nekretnina { get; set; }
+        public int NekretninaId { get; set; }
+        [ForeignKey("KorisnickiNalogId")]
+        public KorisnickiNalog KorisnickiNalog { get; set; }
+        public int KorisnickiNalogId { get; set; }
+   
+
+
     }
 }
