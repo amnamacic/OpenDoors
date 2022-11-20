e8ca5523d22081384482aa61b525ec19d69583e6
aminaM24 <94532479+aminaM24@users.noreply.github.com>
2022-11-20 16:29:43 +0100

    Update Email.cs

--- Seminarski/OpenDoors/Models/Email.cs
+++ Seminarski/OpenDoors/Models/Email.cs
@@ -1,6 +1,17 @@
-﻿namespace OpenDoors.Models
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace IznajmljivanjeNekretnina.Models
 {
     public class Email
     {
+        [Key]
+        public int Id { get; set; }
+        public string Sadrzaj { get; set; }
+        public string Predmet { get; set; }
+        [ForeignKey("KorisnickiNalogId")]
+        public KorisnickiNalog KorisnickiNalog { get; set; }
+        public int KorisnickiNalogId { get; set; }
     }
 }
+
