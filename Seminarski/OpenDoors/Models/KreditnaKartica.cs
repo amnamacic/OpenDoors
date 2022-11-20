14727ecbea38186b0dff2c68e9cc9891d9157042
aminaM24 <94532479+aminaM24@users.noreply.github.com>
2022-11-20 16:32:06 +0100

    Update KreditnaKartica.cs

--- Seminarski/OpenDoors/Models/KreditnaKartica.cs
+++ Seminarski/OpenDoors/Models/KreditnaKartica.cs
@@ -1,6 +1,19 @@
-﻿namespace OpenDoors.Models
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OpenDoors.Models
 {
     public class KreditnaKartica
     {
+        [Key]
+        public int Id { get; set; }
+        public int BrojKartice { get; set; }
+        public string TipKartice { get; set; }
+        public bool Validna { get; set; }
+        public string Status { get; set; }
+        [ForeignKey("KorisnikId")]
+        public Korisnik Korisnik { get; set; }
+        public int KorisnikId { get; set; }
     }
 }
+
