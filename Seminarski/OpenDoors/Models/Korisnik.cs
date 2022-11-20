bd0b027ea0c60dc0625e46ac7c58cf07f299cfdd
aminaM24 <94532479+aminaM24@users.noreply.github.com>
2022-11-20 16:31:23 +0100

    Update Korisnik.cs

--- Seminarski/OpenDoors/Models/Korisnik.cs
+++ Seminarski/OpenDoors/Models/Korisnik.cs
@@ -1,6 +1,24 @@
-﻿namespace OpenDoors.Models
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace IznajmljivanjeNekretnina.Models
 {
-    public class Korisnik
+    [Table("Korisnik")]
+    
+    public class Korisnik : KorisnickiNalog
     {
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public string Spol { get; set; }
+        public DateTime DatumRodjenja { get; set; }
+        public string BrojTelefona { get; set; }
+        [ForeignKey("GradId")]
+        public Grad Grad { get; set; }
+        public int GradId { get; set; }
+        public List<PromoKodovi> PromoKodovi { get; set; }
+        public List<KreditnaKartica> KreditnaKartica { get; set; }
+        public List<Rezervacija> Rezervacija { get; set; }
+        public List<Transakcija> Transakcija { get; set; }
     }
 }
+
