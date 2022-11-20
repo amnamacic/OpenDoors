de1f982b2e20b256186a0c6e0fdb1da6991cea87
aminaM24 <94532479+aminaM24@users.noreply.github.com>
2022-11-20 16:33:08 +0100

    Update Korisnik.cs

--- Seminarski/OpenDoors/Models/Korisnik.cs
+++ Seminarski/OpenDoors/Models/Korisnik.cs
@@ -1,7 +1,7 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace IznajmljivanjeNekretnina.Models
+namespace OpenDoors.Models
 {
     [Table("Korisnik")]
     
