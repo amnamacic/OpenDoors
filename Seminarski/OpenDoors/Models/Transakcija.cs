1da0303093a0ea6f8f9bbcca0f46a45ff4e7d15a
Amna Macić <106256903+amnamacic@users.noreply.github.com>
2022-11-20 21:04:52 +0100

    Update Transakcija.cs

--- Seminarski/OpenDoors/Models/Transakcija.cs
+++ Seminarski/OpenDoors/Models/Transakcija.cs
@@ -1,6 +1,28 @@
-﻿namespace OpenDoors.Models
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OpenDoors.Models
 {
     public class Transakcija
     {
+        [Key]
+        public int Id { get; set; }
+        public string Iznos { get; set; }
+        public string Popust { get; set; }
+        public string NacinPlacanja { get; set; }
+        [ForeignKey("NekretninaId")]
+        public Nekretnina Nekretnina { get; set; }
+        public int NekretninaId { get; set; }
+        [ForeignKey("KorisnikId")]
+        public Korisnik Korisnik { get; set; }
+        public int KorisnikId { get; set; }
+        [ForeignKey("KreditnaKarticaId")]
+        public KreditnaKartica KreditnaKartica { get; set; }
+        public int KreditnaKarticaId { get; set; }
+        [ForeignKey("RezervacijaId")]
+        public Rezervacija Rezervacija { get; set; }
+        public int RezervacijaId { get; set; }
+
+
     }
 }
