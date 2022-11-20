1fdeffbcbc0b8ed45b9a8c6eb4f76d93e6803543
Amna Macić <106256903+amnamacic@users.noreply.github.com>
2022-11-20 21:03:30 +0100

    Update Rezervacija.cs

--- Seminarski/OpenDoors/Models/Rezervacija.cs
+++ Seminarski/OpenDoors/Models/Rezervacija.cs
@@ -1,6 +1,27 @@
-﻿namespace OpenDoors.Models
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OpenDoors.Models
 {
     public class Rezervacija
     {
+        [Key]
+        public int Id { get; set; }
+        public DateTime DatumRezervacije { get; set; }
+        public int BrojOsoba { get; set; }
+        public bool Djeca { get; set; }
+        public string Status { get; set; }
+        public DateTime DatumOtkazivanja { get; set; }
+        public double Cijena { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+        public bool PovratNovca { get; set; }
+        [ForeignKey("NekretninaId")]
+        public Nekretnina Nekretnina { get; set; }
+        public int NekretninaId { get; set; }
+        [ForeignKey("KorisnikId")]
+        public Korisnik Korisnik { get; set; }
+        public int KorisnikId { get; set; }
+        public List<Transakcija> Transakcija { get; set; }
     }
 }
