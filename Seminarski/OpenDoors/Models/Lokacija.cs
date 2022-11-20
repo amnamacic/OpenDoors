56545be37eda99f54e6cf8a61888461ca1b3ee63
aminaM24 <94532479+aminaM24@users.noreply.github.com>
2022-11-20 16:33:57 +0100

    Update Lokacija.cs

--- Seminarski/OpenDoors/Models/Lokacija.cs
+++ Seminarski/OpenDoors/Models/Lokacija.cs
@@ -1,6 +1,18 @@
-﻿namespace OpenDoors.Models
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OpenDoors.Models
 {
     public class Lokacija
     {
+        [Key]
+        public int Id { get; set; }
+        public string DioGrada { get; set; }
+        public string Naziv { get; set; }
+        [ForeignKey("GradId")]
+        public Grad Grad { get; set; }
+        public int GradId { get; set; }
+        public List<Nekretnina> Nekretnina { get; set; }
     }
 }
+
