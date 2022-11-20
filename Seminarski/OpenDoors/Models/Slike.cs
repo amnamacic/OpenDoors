85a360d5f3d13530238740a84e09e6f6f78d75d0
Amna Macić <106256903+amnamacic@users.noreply.github.com>
2022-11-20 21:03:56 +0100

    Update Slike.cs

--- Seminarski/OpenDoors/Models/Slike.cs
+++ Seminarski/OpenDoors/Models/Slike.cs
@@ -1,6 +1,16 @@
-﻿namespace OpenDoors.Models
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OpenDoors.Models
 {
     public class Slike
     {
+        [Key]
+        public int Id { get; set; }
+        public DateTime DatumPostavljanja { get; set; }
+        public byte[] Slika { get; set; }
+       [ForeignKey("OpisId")]
+        public OpisNekretnine OpisNekretnine { get; set; }
+        public int OpisId { get; set; }
     }
 }
