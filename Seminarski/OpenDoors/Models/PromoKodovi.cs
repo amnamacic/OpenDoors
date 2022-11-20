52f517e0d0c988aebe8da57a011ba9c91d032dff
Amna Macić <106256903+amnamacic@users.noreply.github.com>
2022-11-20 21:02:31 +0100

    Update PromoKodovi.cs

--- Seminarski/OpenDoors/Models/PromoKodovi.cs
+++ Seminarski/OpenDoors/Models/PromoKodovi.cs
@@ -1,6 +1,17 @@
-﻿namespace OpenDoors.Models
-{
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OpenDoors.Models
+{ 
     public class PromoKodovi
     {
+        [Key]
+        public int Id { get; set; }
+        public string Opis { get; set; }
+        public string Popust { get; set; }
+        public string Naziv { get; set; }
+        public string Kod { get; set; }
+        public bool Validan { get; set; }
+        public List<KrajnjiKorisnik> KrajnjiKorisnik { get; set; }
     }
 }
