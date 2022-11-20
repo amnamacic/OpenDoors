6549693140af81f38f221e7e25dc61b9f7c4d1fe
aminaM24 <94532479+aminaM24@users.noreply.github.com>
2022-11-20 16:30:34 +0100

    Update Grad.cs

--- Seminarski/OpenDoors/Models/Grad.cs
+++ Seminarski/OpenDoors/Models/Grad.cs
@@ -1,6 +1,15 @@
-﻿namespace OpenDoors.Models
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OpenDoors.Models
 {
     public class Grad
     {
+        [Key]
+        public int Id { get; set; }
+        public string Naziv { get; set; }
+        public int PostanskiBroj { get; set; }
+        public List<Lokacija> Lokacija { get; set; }
+        public List<Korisnik> Korisnik { get; set; }
     }
 }
