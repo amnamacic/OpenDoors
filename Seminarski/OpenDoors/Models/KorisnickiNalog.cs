d065f214ce99c70372e62a5de4c408157e0becda
aminaM24 <94532479+aminaM24@users.noreply.github.com>
2022-11-20 16:31:01 +0100

    Update KorisnickiNalog.cs

--- Seminarski/OpenDoors/Models/KorisnickiNalog.cs
+++ Seminarski/OpenDoors/Models/KorisnickiNalog.cs
@@ -1,6 +1,22 @@
-﻿namespace OpenDoors.Models
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace IznajmljivanjeNekretnina.Models
 {
+    [Table("KorisnickiNalog")]
     public class KorisnickiNalog
     {
+        [Key]
+        
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public bool Verifikovan { get; set; }
+        public List<Recenzije> Recenzije { get; set; }
+        public List<Notifikacija> Notifikacija { get; set; }
+        public List<Email> Emails { get; set; }
+
     }
 }
+
