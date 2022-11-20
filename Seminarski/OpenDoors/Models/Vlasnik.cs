a10fa4bc090b65d07c536e84107619a9594022cd
Amna Macić <amna.macic@edu.fit.ba>
2022-11-20 20:46:22 +0100

    AddClasses

---
+++ Seminarski/OpenDoors/Models/Vlasnik.cs
@@ -0,0 +1,14 @@
+﻿using OpenDoors.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OpenDoors.Models
+{
+    [Table("Vlasnik")]
+    public class Vlasnik : Korisnik
+    {
+        public int BrojNekretnina { get; set; }
+        public DateTime IznajmljivateljOd { get; set; }
+        public string Titula { get; set; }
+        public List<Nekretnina> Nekretnina { get; set; }
+    }
+}
